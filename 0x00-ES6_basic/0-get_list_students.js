d866f6b738540fb7bf65b273759e783470598ad3
sirln <siro@sirlawren.com>
2024-01-10 02:53:13 +0300

    Basic list of objects

---
+++ 0x00-ES6_basic/0-get_list_students.js
@@ -0,0 +1,9 @@
+// Function that returns an array of objects.
+function getListStudents() {
+    const objects = [
+            { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+            { id: 2, firstName: 'James', location: 'Columbia' },
+            { id: 5, firstName: 'Serena', location: 'San Francisco' }
+        ];
+    return objects;
+}
