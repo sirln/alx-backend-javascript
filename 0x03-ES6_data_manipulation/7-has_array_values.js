64d57417e7d5aef456a61ba731f647b3e3d4374e
sirln <siro@sirlawren.com>
2024-01-10 05:52:28 +0300

    More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,7 +1,7 @@
 // Function that returns a boolean
 // if all the elements in the array exist within the set.
 function hasValuesFromArray(set, array) {
-  const status = array.every(element => set.has(element));
+  const status = array.every((element) => set.has(element));
   return status;
 }
 
