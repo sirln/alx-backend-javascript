f59c91464ee763a5255c031d6d0cfe9363db2a25
sirln <siro@sirlawren.com>
2024-01-10 07:08:33 +0300

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,20 @@
+// Export a const instance of WeakMap and name it weakMap.
+
+const weakMap = new WeakMap();
+
+
+function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const currentCount = weakMap.get(endpoint);
+  weakMap.set(endpoint, currentCount + 1);
+
+  if (weakMap.get(endpoint) >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
+
+export { weakMap };
+export { queryAPI };
