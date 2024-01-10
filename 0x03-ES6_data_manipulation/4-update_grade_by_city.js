8e0cd516f528f059247e79f7c2dd4b13ecef99d4
sirln <siro@sirlawren.com>
2024-01-10 05:22:25 +0300

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,13 @@
 // Function that returns an array of students for a specific city with their new grade
 function updateStudentGradeByCity(students, city, newGrades) {
-  const newStudentGrades = students.filter(student => student.location === city)
-    .map(student => {
-      const studentGrade = newGrades.find(grade => grade.studentId === student.id);
+  const newStudentGrades = students.filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
 
       if (studentGrade) {
         return { ...student, grade: studentGrade.grade };
-      } else {
-        return { ...student, grade: 'N/A' };
       }
+      return { ...student, grade: 'N/A' };
     });
   return newStudentGrades;
 }
